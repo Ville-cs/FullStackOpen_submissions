ebb68e51a21669e36f9001ee8527f460804b4353
Ville-cs <92691355+Ville-cs@users.noreply.github.com>
2024-09-27 14:44:30 +0300

    Updated frontend directory

---
+++ part3/phonebook-frontend/src/components/ShowEntries.jsx
@@ -0,0 +1,34 @@
+import noteService from "../services/noteService"
+
+const ShowEntries = ({ newSearch, persons, setPersons }) => {
+  const whoToShow = !newSearch
+  ? persons
+  : persons.filter(person =>
+    person.name.toLowerCase().includes(newSearch.toLowerCase()))
+
+  const handleDelete = (id) => {
+    if (window.confirm("Delete this number?")) {
+      noteService
+        .remove(id)
+        .then(
+          console.log("Deleted entry"),
+          setPersons(persons.filter(person => person.id !== id))
+        )
+        .catch(error =>
+          console.log("Error while deleting")
+        )
+    }
+  }
+
+  return (
+    <div>
+      {whoToShow.map(person =>
+        <div key={person.id}>
+          {person.name} {person.number}
+          <button onClick={() => handleDelete(person.id)}>delete</button>
+        </div>)}
+    </div>
+  )
+}
+
+export default ShowEntries
