2817c6e31397e6b5d8a256926ba4040e452a6614
Ville-cs <92691355+Ville-cs@users.noreply.github.com>
2024-10-10 18:49:50 +0300

    error message for duplicate username

--- part4/blogApp/utils/middleware.js
+++ part4/blogApp/utils/middleware.js
@@ -11,6 +11,13 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (
+    error.name === 'MongoServerError' &&
+    error.message.includes('E11000 duplicate key error')
+  ) {
+    return response
+      .status(400)
+      .json({ error: 'expected `username` to be unique' })
   }
 
   next(error)
@@ -18,5 +25,5 @@ const errorHandler = (error, request, response, next) => {
 
 module.exports = {
   unknownEndpoint,
-  errorHandler
+  errorHandler,
 }
