edd90d113188745792f73f2b9cea3d2793d5d30e
Ville-cs <92691355+Ville-cs@users.noreply.github.com>
2024-10-10 18:49:23 +0300

    enabled user middleware

--- part4/blogApp/app.js
+++ part4/blogApp/app.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 require('express-async-errors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -18,11 +19,11 @@ mongoose
   .then(() => {
     logger.info('connected to mongodb')
   })
-  .catch((error) => {
+  .catch(error => {
     logger.error('error while connecting to mongodb', error.message)
   })
 
-morgan.token('body', (req) => {
+morgan.token('body', req => {
   return JSON.stringify(req.body)
 })
 
@@ -33,6 +34,7 @@ app.use(express.json())
 //   morgan(':method :url :status :res[content-length] - :response-time ms :body ')
 // )
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
