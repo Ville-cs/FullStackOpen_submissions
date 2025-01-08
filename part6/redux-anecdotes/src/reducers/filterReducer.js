1a3b82d46ecbb4e1e272fdac994432dbdfcc083a
Ville-cs <92691355+Ville-cs@users.noreply.github.com>
2025-01-08 16:57:53 +0200

    refactored filter to use redux toolkit

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,20 +1,36 @@
-const filterReducer = (state, action) => {
-  if (!state) {
-    state = ''
-  }
-  switch (action.type) {
-    case 'SET_FILTER':
+import { createSlice } from '@reduxjs/toolkit'
+
+// const filterReducer = (state, action) => {
+//   if (!state) {
+//     state = ''
+//   }
+//   switch (action.type) {
+//     case 'SET_FILTER':
+//       return action.payload
+//     default:
+//       return state
+//   }
+// }
+
+// export const filter = filter => {
+//   return {
+//     type: 'SET_FILTER',
+//     payload: filter,
+//   }
+// }
+
+const initialState = ''
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    setFilter(state, action) {
+      // state = action.payload
       return action.payload
-    default:
-      return state
-  }
-}
+    },
+  },
+})
 
-export const filter = filter => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter,
-  }
-}
-
-export default filterReducer
+export const { setFilter } = filterSlice.actions
+export default filterSlice.reducer
