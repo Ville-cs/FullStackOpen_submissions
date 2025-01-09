7d8536385753e76ef9fefebb40c714f1e16094a9
Ville-cs <92691355+Ville-cs@users.noreply.github.com>
2025-01-09 20:17:11 +0200

    added db.json and axios requests

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,15 +1,18 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { createMessage, deleteMessage } from '../reducers/messageReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const newAnecdote = event => {
+  const postAnecdote = async event => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
+    const newAnecdote = await anecdoteService.addNew(content)
+    dispatch(createAnecdote(newAnecdote))
+
     dispatch(createMessage(`You posted ${content}`))
     setTimeout(() => {
       dispatch(deleteMessage())
@@ -19,7 +22,7 @@ const AnecdoteForm = () => {
   return (
     <div>
       <h2>create anecdote</h2>
-      <form onSubmit={newAnecdote}>
+      <form onSubmit={postAnecdote}>
         <div>
           <input name="anecdote" />
         </div>
