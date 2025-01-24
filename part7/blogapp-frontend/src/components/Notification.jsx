b8d43f52d90eeb3efede99019558d08ab599be64
Ville-cs <92691355+Ville-cs@users.noreply.github.com>
2025-01-24 19:37:34 +0200

    added notification context and reducer

--- part7/blogapp-frontend/src/components/Notification.jsx
+++ part7/blogapp-frontend/src/components/Notification.jsx
@@ -1,8 +1,16 @@
+import { useContext } from 'react'
+import NotificationContext from '../NotificationContext'
+
 const Notification = ({ errorMessage, message }) => {
+  const [notification, dispatch] = useContext(NotificationContext)
+
   if (errorMessage) {
     return <div className="errorStyle">{errorMessage}</div>
-  } else if (message) {
-    return <div className="messageStyle">{message}</div>
+  } else if (notification) {
+    setTimeout(() => {
+      dispatch({ type: 'ERASE' })
+    }, 5000)
+    return <div className="messageStyle">{notification}</div>
   }
 
   return null
