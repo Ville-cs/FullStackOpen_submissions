cea9f997f30889b92c214bea7f3d3877a284daef
Ville-cs <92691355+Ville-cs@users.noreply.github.com>
2025-02-10 21:55:07 +0200

    initial commit of bookapp frontend

---
+++ part8/library-frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+import { Routes, Route, Link } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <Link to="/">
+          <button>authors</button>
+        </Link>
+        <Link to="/books">
+          <button>books</button>
+        </Link>
+        <Link to="/newbook">
+          <button>add book</button>
+        </Link>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Authors />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/newbook" element={<NewBook />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
