738ec14e77967fab76b49072adcd28fa11409132
Ville-cs <92691355+Ville-cs@users.noreply.github.com>
2025-03-01 17:23:01 +0200

    added post method to endpoint /api/patients

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from 'express';
 import patientService from '../services/patientService';
 
@@ -6,6 +7,19 @@ const patientsRouter = express.Router();
 patientsRouter.get('/', (_req, res) => {
   res.status(200).json(patientService.getPatients());
   return;
+});
+
+patientsRouter.post('/', (req, res) => {
+  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+  const addEntry = patientService.addPatient({
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation,
+  });
+
+  res.status(201).json(addEntry);
 });
 
 export default patientsRouter;
--- part9/patientor-backend/src/services/patientService.ts
+++ part9/patientor-backend/src/services/patientService.ts
@@ -1,5 +1,7 @@
 import patients from '../data/patients';
-import { PatientEntry } from '../types';
+import { PatientEntry, NewPatientEntry } from '../types';
+import { v1 as uuid } from 'uuid';
+const id = uuid();
 
 const getPatients = () => {
   return patients.map(
@@ -13,6 +15,16 @@ const getPatients = () => {
   );
 };
 
+const addPatient = (entry: NewPatientEntry) => {
+  const newEntry = {
+    id: id,
+    ...entry,
+  };
+  patients.push(newEntry);
+  return newEntry;
+};
+
 export default {
   getPatients,
+  addPatient,
 };
