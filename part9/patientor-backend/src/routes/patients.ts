6aee654a5359ac056913577fc7cdb987e128a7fa
Ville-cs <92691355+Ville-cs@users.noreply.github.com>
2025-03-02 15:51:44 +0200

    refactored post validation to use zod schema

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import patientService from '../services/patientService';
-import { toNewPatientEntry } from '../utils/newPatientValidator';
+import { z } from 'zod';
+import { newPatientSchema } from '../utils/newPatientSchema';
 
 const patientsRouter = express.Router();
 
@@ -11,12 +12,14 @@ patientsRouter.get('/', (_req, res) => {
 
 patientsRouter.post('/', (req, res) => {
   try {
-    const newPatientEntry = toNewPatientEntry(req.body);
+    const newPatientEntry = newPatientSchema.parse(req.body);
     const addedEntry = patientService.addPatient(newPatientEntry);
     res.status(201).json(addedEntry);
   } catch (error: unknown) {
-    if (error instanceof Error) {
-      res.status(400).send('Error ' + error.message);
+    if (error instanceof z.ZodError) {
+      res.status(400).send({ error: error.issues });
+    } else {
+      res.status(400).send({ error: 'unknown error' });
     }
   }
 });
