9aeac1a92bdb7e1c485eb9bd1de5797c45a70b1a
Ville-cs <92691355+Ville-cs@users.noreply.github.com>
2025-03-10 16:16:02 +0200

    added gender a icon for the patient view page

--- part9/patientor-frontend/src/components/PatientPage/index.tsx
+++ part9/patientor-frontend/src/components/PatientPage/index.tsx
@@ -1,14 +1,22 @@
 import { PatientProps } from '../../types';
+import MaleIcon from '@mui/icons-material/Male';
+import FemaleIcon from '@mui/icons-material/Female';
 
 const PatientPage = (props: PatientProps) => {
   const { patient } = props;
-  console.log(patient);
 
   if (!patient) return;
 
   return (
     <div>
       <h2>{patient.name}</h2>
+      {patient.gender === 'male' ? (
+        <MaleIcon />
+      ) : patient.gender === 'female' ? (
+        <FemaleIcon />
+      ) : (
+        'gender not specified'
+      )}
       <p>ssn: {patient.ssn}</p>
       <p>occupation: {patient.occupation}</p>
     </div>
