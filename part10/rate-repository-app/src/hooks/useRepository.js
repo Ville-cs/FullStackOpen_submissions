f4eae073071237f1d4a591cf7f290c5d41ad1d1d
Ville <ville-poikela@hotmail.com>
2026-08-06 20:35:37 +0300

    adds useRepository hook

---
+++ part10/rate-repository-app/src/hooks/useRepository.js
@@ -0,0 +1,17 @@
+import { useQuery } from "@apollo/client/react";
+import { GET_REPOSITORY } from "../graphql/queries";
+
+const useRepository = (id) => {
+  const { data, loading, refetch, error } = useQuery(GET_REPOSITORY, {
+    variables: { repositoryId: id },
+    fetchPolicy: "cache-and-network",
+  });
+  return {
+    repository: data?.repository,
+    loading,
+    error,
+    refetch,
+  };
+};
+
+export default useRepository;
