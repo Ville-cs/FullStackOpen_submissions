1f1c28f387dc4c02da391a24e320d8d99b10743d
Ville <ville-poikela@hotmail.com>
2026-08-06 19:20:55 +0300

    implements a button to open related repository in GitHub

--- part10/rate-repository-app/src/components/RepositoryList/SingleRepository.jsx
+++ part10/rate-repository-app/src/components/RepositoryList/SingleRepository.jsx
@@ -1,10 +1,11 @@
-import { View, ActivityIndicator } from "react-native";
+import { View, ActivityIndicator, Pressable, StyleSheet } from "react-native";
 import RepositoryItemInfo from "./RepositoryItemInfo";
 import RepositoryItemStats from "./RepositoryItemStats";
 import { useParams } from "react-router";
 import { useQuery } from "@apollo/client/react";
 import { GET_REPOSITORY } from "../../graphql/queries";
 import Text from "../Text";
+import * as Linking from "expo-linking";
 
 const SingleRepository = () => {
   const { id } = useParams();
@@ -24,12 +25,36 @@ const SingleRepository = () => {
   }
   const repo = data.repository;
 
+  const handlePress = () => {
+    Linking.openURL(repo.url);
+  };
+
   return (
     <View>
       <RepositoryItemInfo item={repo} />
       <RepositoryItemStats item={repo} />
+      <Pressable onPress={handlePress}>
+        <Text
+          fontWeight="bold"
+          fontSize="subheading"
+          backgroundColor="blue"
+          color="textSecondary"
+          style={styles.button}
+        >
+          Open in GitHub
+        </Text>
+      </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 30,
+    textAlign: "center",
+    paddingVertical: 20,
+    borderRadius: 10,
+  },
+});
 
 export default SingleRepository;
