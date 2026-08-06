0b9f103d0515a28db620a75c0627942333af7d84
Ville <ville-poikela@hotmail.com>
2026-08-06 13:29:21 +0300

    adds data-testId attirbute

--- part10/rate-repository-app/src/components/RepositoryList/RepositoryItem.jsx
+++ part10/rate-repository-app/src/components/RepositoryList/RepositoryItem.jsx
@@ -4,7 +4,7 @@ import RepositoryItemInfo from "./RepositoryItemInfo";
 
 const RepositoryItem = ({ item }) => {
   return (
-    <View style={styles.margin} testId="repositoryItem">
+    <View style={styles.margin} data-testId="repositoryItem">
       <RepositoryItemInfo item={item} />
       <RepositoryItemStats item={item} />
     </View>
