58322b8ef948fabe85e33bf76e656adc03d3e2d1
Ville <ville-poikela@hotmail.com>
2026-08-08 10:24:08 +0300

    adds NativeRouter to test case to handle component navigation

--- part10/rate-repository-app/src/__tests__/components/RepositoryList.test.jsx
+++ part10/rate-repository-app/src/__tests__/components/RepositoryList.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen, within } from "@testing-library/react-native";
 import { RepositoryListContainer } from "../../components/RepositoryList";
+import { NativeRouter } from "react-router-native";
 
 describe("RepositoryList", () => {
   describe("RepositoryListContainer", () => {
@@ -47,7 +48,11 @@ describe("RepositoryList", () => {
         ],
       };
 
-      await render(<RepositoryListContainer repositories={repositories} />);
+      await render(
+        <NativeRouter>
+          <RepositoryListContainer repositories={repositories} />
+        </NativeRouter>,
+      );
       const repositoryItems = screen.getAllByTestId("repositoryItem");
       expect(repositoryItems).toHaveLength(2);
       repositoryItems.forEach((item, index) => {
