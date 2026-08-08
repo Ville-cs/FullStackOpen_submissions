28a7ed3da2d41cc0332fda580bbc8e172ee55ccd
Ville <ville-poikela@hotmail.com>
2026-08-08 15:01:09 +0300

    adds filtering by keyword to useRepositories hook

--- part10/rate-repository-app/src/hooks/useRepositories.js
+++ part10/rate-repository-app/src/hooks/useRepositories.js
@@ -1,11 +1,12 @@
 import { useQuery } from "@apollo/client/react";
 import { GET_REPOSITORIES } from "../graphql/queries";
 
-const useRepositories = ({ orderBy, orderDirection } = {}) => {
+const useRepositories = ({ orderBy, orderDirection, searchKeyword } = {}) => {
   const { data, loading, refetch, error } = useQuery(GET_REPOSITORIES, {
     variables: {
       orderBy,
       orderDirection,
+      searchKeyword,
     },
     fetchPolicy: "cache-and-network",
   });
