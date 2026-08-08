9853ed0698bb3ae141ce792d42f964e3841ae9af
Ville <ville-poikela@hotmail.com>
2026-08-08 11:04:25 +0300

    params are now stored in an object

--- part10/rate-repository-app/src/hooks/useRepositories.js
+++ part10/rate-repository-app/src/hooks/useRepositories.js
@@ -1,8 +1,12 @@
 import { useQuery } from "@apollo/client/react";
 import { GET_REPOSITORIES } from "../graphql/queries";
 
-const useRepositories = () => {
+const useRepositories = ({ order, direction }) => {
   const { data, loading, refetch, error } = useQuery(GET_REPOSITORIES, {
+    variables: {
+      orderBy: order,
+      orderDirection: direction,
+    },
     fetchPolicy: "cache-and-network",
   });
 
