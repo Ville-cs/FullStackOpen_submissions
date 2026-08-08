a527e2056bac63108edfff45e13e94d4b51531b5
Ville <ville-poikela@hotmail.com>
2026-08-08 12:02:36 +0300

    changes param names and makes them optional

--- part10/rate-repository-app/src/hooks/useRepositories.js
+++ part10/rate-repository-app/src/hooks/useRepositories.js
@@ -1,11 +1,11 @@
 import { useQuery } from "@apollo/client/react";
 import { GET_REPOSITORIES } from "../graphql/queries";
 
-const useRepositories = ({ order, direction }) => {
+const useRepositories = ({ orderBy, orderDirection } = {}) => {
   const { data, loading, refetch, error } = useQuery(GET_REPOSITORIES, {
     variables: {
-      orderBy: order,
-      orderDirection: direction,
+      orderBy,
+      orderDirection,
     },
     fetchPolicy: "cache-and-network",
   });
